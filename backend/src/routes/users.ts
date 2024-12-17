038937c59c8b19db3bbf1fe5777aa54503de8616
pr0fix <bgx152@myy.haaga-helia.fi>
2024-12-17 08:53:35 +0200

    Add functionalities for updating & deleting users

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,6 +4,7 @@ import {
   authenticateToken,
 } from "../middleware/authMiddleware";
 import userService from "../services/userService";
+import { IUser } from "../utils/types";
 
 const router = express.Router();
 
@@ -34,6 +35,49 @@ router.get(
         return;
       }
       res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
+
+router.put(
+  "/users/:id",
+  authenticateToken,
+  authenticateRole,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const userData: Partial<IUser> = req.body;
+    try {
+      const updatedUser = await userService.editUser(id, userData);
+      if (!updatedUser) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+      res.status(200).json({ message: "User updated successfully" });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+        return;
+      }
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
+
+router.delete(
+  "/users/:id",
+  authenticateToken,
+  authenticateRole,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const success = await userService.deleteUser(id);
+      if (!success) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+      res.status(200).json({ message: "User deleted successfully" });
     } catch (error) {
       res.status(500).json({ error: "Internal server error" });
     }
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,4 +1,5 @@
 import User from "../models/user";
+import { IUser } from "../utils/types";
 
 const getAllUsers = async () => {
   try {
@@ -19,11 +20,48 @@ const getUserById = async (id: string) => {
     return user;
   } catch (error) {
     console.error(`Error fetching user with id ${id}:`, error);
-    throw new Error("Error fetching coffee");
+    throw new Error("Error fetching user");
+  }
+};
+
+const editUser = async (
+  id: string,
+  userData: Partial<IUser>
+): Promise<IUser> => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: userData },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      throw new Error("User not found");
+    }
+
+    return updatedUser;
+  } catch (error) {
+    console.error(`Error updating user with id ${id}:`, error);
+    throw new Error("Error updating user");
+  }
+};
+
+const deleteUser = async (id: string) => {
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  } catch (error) {
+    console.error(`Error deleting user with id ${id}:`, error);
+    throw new Error("Error deleting user");
   }
 };
 
 export default {
   getAllUsers,
   getUserById,
+  editUser,
+  deleteUser,
 };
